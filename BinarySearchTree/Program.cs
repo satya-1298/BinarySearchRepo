13b3a11116f937c726578c62e3259d34aaf9133e
unknown <satya-1298>
2023-04-02 16:15:36 +0530

    Created a BinaryTree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.Add(56);
+            tree.Add(30);
+            tree.Add(70);
+            tree.Add(22);
+            tree.Add(40);
+            tree.Add(11);
+            tree.Add(3);
+            tree.Add(16);
+            tree.Add(60);
+            tree.Add(95);
+            tree.Add(65);
+            tree.Add(63);
+            tree.Add(67);
+
+            Console.WriteLine("PREORDER");
+            tree.TravarsePreorder(tree.Root);
+            Console.WriteLine("\n");
+            Console.WriteLine("POSTORDER");
+            tree.Postorder(tree.Root);
+            Console.WriteLine("\n"); 
+            Console.WriteLine("INORDER");
+            tree.Inorder(tree.Root);
+           
+        }
+    }
+}
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,96 @@
+﻿using BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Tree
+    {
+        public Node Root { get; set; }
+
+        public bool Add(int value)
+        {
+
+            Node before = null, after = this.Root;
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                {
+                    after = after.LeftNode;
+                }
+                else if (value > after.Data)
+                {
+                    after = after.RightNode;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            Node newnode = new Node();
+            newnode.Data = value;
+            if (this.Root == null)
+            {
+                this.Root = newnode;
+
+            }
+            else
+            {
+                if (value < before.Data)
+                {
+                    before.LeftNode = newnode;
+
+                }
+                else
+                {
+                    before.RightNode = newnode;
+                }
+            }
+            return true;
+
+        }
+
+
+        public void TravarsePreorder(Node Parent)
+        {
+            
+            if (Parent != null)
+            {
+                
+                Console.Write(Parent.Data + " ");
+                TravarsePreorder(Parent.LeftNode);
+                TravarsePreorder(Parent.RightNode);
+            }
+        }
+        public void Postorder(Node Parent)
+        {
+            
+            if (Parent != null)
+            {
+                
+                Postorder(Parent.LeftNode);
+                Postorder(Parent.RightNode);
+                Console.Write(Parent.Data + " ");
+            }
+        }
+        public void Inorder(Node Parent)
+        {
+
+            if (Parent != null)
+            {
+
+                Postorder(Parent.LeftNode);
+                Console.Write(Parent.Data + " ");
+                Postorder(Parent.RightNode);
+               
+            }
+        }
+
+    }
+}
