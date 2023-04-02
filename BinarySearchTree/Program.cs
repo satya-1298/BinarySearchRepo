afd62ae2ed83b141f752d061af54b889ed3a4114
unknown <satya-1298>
2023-04-02 09:41:24 +0530

    Creating Binary Tree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Tree tree = new Tree();
+            tree.Add(56);
+            tree.Add(30);
+            tree.Add(70);
+            tree.TravarsePreorder(tree.Root);
+
+        }
+    }
+}
